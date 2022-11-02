a365b04da2c7ba328113cd4a19781d98bd2d6bf5
bombitmanbomb <bombitmanbomb@gmail.com>
2022-11-02 10:42:40 +0000

    Deploying to gh-pages from @ afocommunity/battlemetrics.js@3628c45a816de6708cfcf8899e7df264f977f553 🚀

--- definitions/api/data/APIReservedSlot.d.ts
+++ definitions/api/data/APIReservedSlot.d.ts
@@ -5,4 +5,14 @@ declare interface APIReservedSlot extends APIBase {
    */
   id: `${number}`;
   type: 'reservedSlot';
+  attributes: APIAttributes<{
+    /**Timestamp of when the reserved slot was added */
+    createdAt: string
+    /**When the slot should expire. If null the reserved slot will not expire. */
+    expires?: Nullable<string>
+    /**An array of identifiers to associated with the reserved slot. Can link known identifiers, or manually provided ones. */
+    identifiers: (number | APIIdentifier)[]
+  }>
+  meta?: Nullable<Record<string, unknown>>
+  relationships: APIRelationships<'organization' | 'player' | 'user'> & APIRelationshipsArray<'servers'>
 }
